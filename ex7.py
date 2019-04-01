c48cde77a1e840014a6613f8b02d25f2f1de4815
KritoCC <1240963493@qq.com>
2019-04-01 21:35:31 +0800

    m

---
+++ ex7.py
@@ -0,0 +1,29 @@
+# 习题七
+
+# 普通的打印
+print("Mary had a little lamb.")
+# 格式化字符
+print("Its fleece was white as %s" % 'snow')
+# 普通的打印
+print("And everywhere that Mary went.")
+# 字符的乘法运算，作用是重复打印特定字符n次。
+print("." * 10)  # what'd that do?
+
+# 为下面的打印设置变量
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end.  try removing it to see what happens
+# 打印两个多变量拼接字符
+print(end1 + end2 + end3 + end4 + end5 + end6,
+      end7 + end8 + end9 + end10 + end11 + end12)
