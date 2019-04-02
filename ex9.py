db98097eae6e37efeaf75f57495a9ca596b5f19a
KritoCC <1240963493@qq.com>
2019-04-02 20:08:29 +0800

    ex9.py

---
+++ ex9.py
@@ -0,0 +1,21 @@
+# 习题九
+
+# Here's some new strange stuff, remember type it exactly.
+# 这是一些新奇怪的东西，记得准确输入。
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+mouths = "\nJan \nFeb \nMar \nJun \nJul \nAug"
+# 通过给字符串加上\n实现多行打印
+
+print("Here are the days: ", days)
+print("Here are the mouths:", mouths)
+
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+三引号，可以进行多行编辑
+并且同时可以使用单引号和双引号"''"
+-.-
+""")
