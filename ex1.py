33f84817bebc5b7cceb6bae836ee4a268c075cf9
KritoCC <45055929+KritoCC@users.noreply.github.com>
2019-04-02 15:19:49 +0800

    Delete ex1.py

--- ex1.py
+++
@@ -1,9 +0,0 @@
-# 习题一
-
-print("Hello Again")
-print("I like typing this.")
-print('This is fun')
-print("Yay! Printing.")
-print("I'd much rather you 'not'.")
-print('I "said" do not touch this.')
-print("我爱python。")
