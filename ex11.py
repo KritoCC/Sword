eed9a281c535b1e29febd956fb6d20b0caca3669
KritoCC <1240963493@qq.com>
2019-04-03 16:46:29 +0800

    习题十一

--- ex11.py
+++ ex11.py
@@ -4,7 +4,7 @@ print("How old are you?", end=' ')
 age = input()
 print("How tall are you?", end=' ')
 height = input()
-print("How much da you weigh?", end=' ')
+print("How much do you weigh?", end=' ')
 weight = input()
 
 print(f"So, you're {age} old, {height} tall and {weight} heavy.")
