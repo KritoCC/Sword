e627811b644d4eb53d4c115ec7eb80ec035ab3a4
KritoCC <1240963493@qq.com>
2019-04-04 10:16:07 +0800

    习题十二

---
+++ ex12.py
@@ -0,0 +1,7 @@
+# 习题十二
+
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weight? ")
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
