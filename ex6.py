3b3f05b84ce131d1d1bc7826f33c82145c8c3ce8
“KritoCCgit config user.name “KritoCC <1240963493@qq.com>
2019-05-06 22:54:01 +0800

    习题六

--- ex6.py
+++ ex6.py
@@ -1,26 +1,23 @@
 # 习题六
 
-# 在变量中使用格式化字符
 types_of_people = 10
 x = f"There are {types_of_people} types of people"
-# 在变量中使用格式化字符，并且格式化其他变量
+
 binary = "binary"
 do_not = "dont't"
 y = f"Those who know {binary} and those who {do_not}"
 
 print(x)
 print(y)
-# 打印字符串和格式化的变量
+
 print(f"I said: {x}")
 print(f"I also said: '{y}'")
 
-# 格式化一个布尔型变量False
 hilarious = "False"
 joke_evaluation = "Isn't that joke so funny?! {}"
 
 print(joke_evaluation.format(hilarious))
 
-# 下面演示字符串的拼接打印
 w = "This is the left side of..."
 e = "a string with a right side."
 
